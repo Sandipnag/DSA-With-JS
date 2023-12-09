c747800ac14bbb09857de04bbd94fae62daf04c9
Sandip Nag <35944279+Sandipnag@users.noreply.github.com>
2023-12-09 08:03:40 +0000

    integer occurance done

--- Array/integer_occurance.js
+++ Array/integer_occurance.js
@@ -5,4 +5,12 @@ let arr = [12, 98, 78, 98, 76, 12, 78, 100, 98, 10];
 
 let arr_size = arr.length;
 
-// for(let i=0;)
+for(let i=0;i<arr_size-1;i++){
+    let count = 0;
+    for(j=0;j<arr_size-1;j++){
+        if(arr[i]==arr[j]){
+            count ++
+        }
+    }
+    console.log(`${arr[i]} occurs ${count} times`)
+}
