b7f76857e9bed8b04ef13f472e98e8b58a513c2c
Sandip Nag <35944279+Sandipnag@users.noreply.github.com>
2023-12-09 08:24:56 +0000

    integer occurance optimization done

--- Array/integer_occurance.js
+++ Array/integer_occurance.js
@@ -1,16 +1,19 @@
 /* An array is given with some repeatative integer numbers. Need to know the occurance of those numbers */
 
 
-let arr = [12, 98, 78, 98, 76, 12, 78, 100, 98, 10];
-
+let arr = [12, 98, 78, 98, 76, 12, 78, 100, 98, 10]
+let skip_arr = [];
 let arr_size = arr.length;
 
-for(let i=0;i<arr_size-1;i++){
+for(let i=0;i<=arr_size-1;i++){
     let count = 0;
-    for(j=0;j<arr_size-1;j++){
-        if(arr[i]==arr[j]){
-            count ++
+    if(!skip_arr.includes(arr[i])){
+        for(j=0;j<=arr_size-1;j++){
+            if(arr[i]==arr[j]){
+                skip_arr.push(arr[i])
+                count ++
+            }
         }
+        console.log(`${arr[i]} occurs ${count} times`)
     }
-    console.log(`${arr[i]} occurs ${count} times`)
 }
